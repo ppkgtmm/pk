78ca2dd494866b4fb5ba6fd91a38f405a7151f99
Pinky Gautam <pkgtm1998@gmail.com>
2023-08-22 23:37:10 +0700

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,24 @@
+import { Transition } from "@headlessui/react";
+import Social from "./Social";
+
+function Footer(props) {
+  return (
+    <footer className="bg-black">
+      <div className="mx-4 md:mx-12 lg:mx-[20vw] xl:mx-[25vw] px-4 py-6 lg:px-8 lg:py-16 flex justify-end md:justify-between">
+        <div className="hidden md:block md:visible">
+          <Transition
+            show={props.show}
+            enter="transition-opacity duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+          >
+            <Social />
+          </Transition>
+        </div>
+        <p className="font-light text-white py-[11px]">© 2023 Pinky Gautam</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
