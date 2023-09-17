5bfdaddd5a9c65d1f57e711e15fbac6c10d7b72d
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-17 15:32:36 +0700

    Update Footer.js

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,19 +1,11 @@
-import { Transition } from "@headlessui/react";
 import SocialList from "./SocialList";
 
 function Footer(props) {
   return (
     <footer className="bg-black">
-      <div className="mx-4 md:mx-12 lg:mx-[20vw] xl:mx-[25vw] px-4 py-6 lg:px-8 lg:py-16 flex justify-end md:justify-between">
+      <div className="mx-4 md:mx-12 lg:mx-[20vw] xl:mx-[25vw] px-4 py-10 lg:px-8 lg:py-16 flex justify-end md:justify-between">
         <div className="hidden md:block md:visible">
-          <Transition
-            show={props.showIcons}
-            enter="transition-fade"
-            enterFrom="fade"
-            enterTo="appear"
-          >
-            <SocialList />
-          </Transition>
+          <SocialList />
         </div>
         <p className="font-light text-white py-[11px]">© 2023 Pinky Gautam</p>
       </div>
