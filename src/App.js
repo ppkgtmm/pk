268dd2471900dcd16adc3346f95b552e72fa4b8d
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-18 00:46:50 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import { ProjectList } from "./project";
 import { Cover, Footer } from "./layouts";
+import { Transition } from "@headlessui/react";
+import { CrossIcon } from "./icons";
 import "./index.css";
 
 function App() {
+  const [show, setShow] = useState(true);
   return (
     <div className="app">
-      <Cover />
-      <ProjectList />
-      <Footer />
+      <Transition
+        className="min-h-screen bg-black text-white w-full flex flex-col px-2"
+        show={show}
+      >
+        <button
+          className="self-end p-1 opacity-60 hover:opacity-100 rounded-full absolute top-0 mt-2"
+          href="/"
+          onClick={() => setShow(!show)}
+        >
+          <CrossIcon />
+        </button>
+        <Cover />
+      </Transition>
+      <Transition show={!show}>
+        <ProjectList />
+        <Footer />
+      </Transition>
     </div>
   );
 }
