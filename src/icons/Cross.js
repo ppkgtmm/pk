d5a19a2dab02ddd6cdad4216a5bba4874f45aef3
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-18 00:46:43 +0700

    Update Cross.js

--- src/icons/Cross.js
+++ src/icons/Cross.js
@@ -2,11 +2,17 @@ function Cross() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
       className="icon"
-      fill="currentColor"
-      viewBox="0 0 16 16"
     >
-      <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18L18 6M6 6l12 12"
+      />
     </svg>
   );
 }
