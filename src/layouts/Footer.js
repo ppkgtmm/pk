4e6c6cd30b2698717adb6be2e5859109ebd49161
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-18 00:51:49 +0700

    Update Footer.js

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,6 +1,6 @@
 import SocialList from "./SocialList";
 
-function Footer(props) {
+function Footer() {
   return (
     <footer className="bg-black">
       <div className="mx-4 md:mx-12 lg:mx-[20vw] xl:mx-[25vw] px-4 py-10 lg:px-8 lg:py-16 flex justify-end md:justify-between">
