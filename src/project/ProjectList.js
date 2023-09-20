7bc85244d6f3f94af8b28f4855c029c52899a618
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-20 23:36:57 +0700

    Update ProjectList.js

--- src/project/ProjectList.js
+++ src/project/ProjectList.js
@@ -3,7 +3,7 @@ import data from "./projects.json";
 
 function ProjectList() {
   return (
-    <div className="mx-4 md:mx-12 lg:mx-[20vw] xl:mx-[25vw] px-4 py-12 lg:px-8 lg:py-16 flex flex-col">
+    <div className="component py-12 lg:py-16 flex flex-col">
       <ul className="flex flex-col space-y-10 md:space-y-12">
         {data.map((value, index) => {
           return <Project {...value} key={index} />;
