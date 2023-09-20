28ea32ad04828f837779bbdaf17b3bf4610b1d25
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-20 23:36:50 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,17 +6,8 @@ function App() {
   return (
     <div className="app">
       <Cover />
-
-      {/* <Transition show={!show}> */}
-      {/* <div className="bg-black text-white px-4 md:px-12 lg:px-[20vw] xl:px-[25vw] sticky top-0">
-          <div className="px-4 py-8 lg:px-8 flex justify-between items-center">
-            <span className="text-2xl font-light">Pinky Gautam</span>
-            <SocialList />
-          </div>
-        </div> */}
       <ProjectList />
       <Footer />
-      {/* </Transition> */}
     </div>
   );
 }
