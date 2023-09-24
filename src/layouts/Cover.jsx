6d96376684eb7b0ba99a94899f48b57848110cbb
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-24 18:18:21 +0700

    Update Cover.jsx

--- src/layouts/Cover.jsx
+++ src/layouts/Cover.jsx
@@ -5,11 +5,7 @@ function Cover() {
     <div className="color text-white w-full flex flex-col">
       <div className="component flex flex-col my-12 md:my-20">
         <h1 className="text-[6ch] md:text-[8ch] lg:text-[9ch] font-bold leading-[1.5ch]">
-          Pinky
-          {' '}
-          <br />
-          {' '}
-          Gautam
+          Pinky <br /> Gautam
         </h1>
         <h3 className="mt-4 text-xl text-blue-300">
           data engineer from web development background
