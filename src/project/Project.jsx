adaec1453f7604447646ac8fea4f3ffb2be86fa4
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-24 18:44:09 +0700

    Update Project.jsx

--- src/project/Project.jsx
+++ src/project/Project.jsx
@@ -1,24 +1,36 @@
+import PropTypes from 'prop-types';
+
 function Project(props) {
+  const { name, skills, link, linkText, content, image } = props
   return (
     <li>
       <span className="text-lg">
-        <b className="font-bold">{props.name}</b>
+        <b className="font-bold">{name}</b>
         <span className="font-light"> || </span>
-        {props.skills}
+        {skills}
         <span className="font-light"> || </span>
         <a
           className="underline text-cyan-500"
-          href={props.link}
+          href={link}
           target="_blank"
           rel="noreferrer"
         >
-          {props.linkText}
+          {linkText}
         </a>
       </span>
-      <p className="mt-2 mb-4">{props.content}</p>
-      <img src={props.image} alt={`${props.name} image`} />
+      <p className="mt-2 mb-4">{content}</p>
+      <img src={image} alt={`${name} image`} />
     </li>
   );
 }
+
+Project.propTypes = {
+  name: PropTypes.string.isRequired,
+  skills: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  linkText: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+};
 
 export default Project;
