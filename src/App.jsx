575743e6a57158624fa91e30b014a9857fd41b55
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-02 01:10:51 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import ChurnPrediction from './pages/ChurnPrediction';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/churn-prediction" element={<ChurnPrediction />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
 
   );
 }
