6e8ace40c15622c54c96727d8f1fe895f5bc9342
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-01 22:54:32 +0700

    Update Project.jsx

--- src/project/Project.jsx
+++ src/project/Project.jsx
@@ -2,37 +2,28 @@ import PropTypes from 'prop-types';
 
 function Project(props) {
   const {
-    name, skills, link, linkText, content, image,
-  } = props;
+    name, image,
+  } = props; /* skills, link, linkText, content, */
 
   return (
-    <li>
-      <span className="text-lg">
-        <b className="font-bold">{name}</b>
-        <span className="font-light"> || </span>
-        {skills}
-        <span className="font-light"> || </span>
-        <a
-          className="underline text-cyan-500"
-          href={link}
-          target="_blank"
-          rel="noreferrer"
-        >
-          {linkText}
-        </a>
-      </span>
-      <p className="mt-2 mb-4">{content}</p>
-      <img src={image} alt={name} />
-    </li>
+    <div className="">
+      <div>
+        <img src={image} alt={name} />
+      </div>
+      <div>
+        <p className="font-bold text-lg lowercase my-2">{name}</p>
+        <a href="/" className="text-neutral-900 border border-neutral-900 hover:bg-neutral-900 hover:text-white capitalize px-3 py-1.5 rounded">read more</a>
+      </div>
+    </div>
   );
 }
 
 Project.propTypes = {
   name: PropTypes.string.isRequired,
-  skills: PropTypes.string.isRequired,
-  link: PropTypes.string.isRequired,
-  linkText: PropTypes.string.isRequired,
-  content: PropTypes.string.isRequired,
+  // skills: PropTypes.string.isRequired,
+  // link: PropTypes.string.isRequired,
+  // linkText: PropTypes.string.isRequired,
+  // content: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
 };
 
