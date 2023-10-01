c9055edc6f376ceb9740690bf8d7720d40ba2d5d
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-01 23:51:06 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="app">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/iii" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
