14b6a89ba0843c9c60748b05dd175e30622ae3a1
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-01 19:13:38 +0700

    Update Cover.jsx

--- src/layouts/Cover.jsx
+++ src/layouts/Cover.jsx
@@ -1,8 +1,6 @@
-import SocialList from './SocialList';
-
 function Cover() {
   return (
-    <div className="color text-white w-full flex flex-col">
+    <div className="w-full flex flex-col">
       <div className="component flex flex-col my-12 md:my-20">
         <h1 className="text-[6ch] md:text-[8ch] lg:text-[9ch] font-bold leading-[1.5ch]">
           Pinky
@@ -11,16 +9,10 @@ function Cover() {
           {' '}
           Gautam
         </h1>
-        <h3 className="mt-4 text-xl text-blue-300">
-          data engineer from web development background
-        </h3>
-        <p className="mt-8 md:mt-10 font-light">
-          eager to learn and implement highly maintainable solutions that
-          support informed decision making and bring about positive impacts
-        </p>
-        <div className="mt-6 md:hidden">
-          <SocialList />
-        </div>
+        <blockquote className="mt-8 quote">
+          data engineer who is eager to learn and implement highly maintainable solutions
+          that support informed decision making and bring about positive impacts
+        </blockquote>
       </div>
     </div>
   );
