508067324c942c75b6785bd35d1cfad7c6fd7ee4
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-01 20:18:23 +0700

    Update Cover.jsx

--- src/layouts/Cover.jsx
+++ src/layouts/Cover.jsx
@@ -1,6 +1,6 @@
 function Cover() {
   return (
-    <div className="w-full flex flex-col">
+    <div className="w-full flex flex-col bg-zinc-900 text-white">
       <div className="component flex flex-col my-12 md:my-20">
         <h1 className="text-[6ch] md:text-[8ch] lg:text-[9ch] font-bold leading-[1.5ch]">
           Pinky
@@ -9,10 +9,10 @@ function Cover() {
           {' '}
           Gautam
         </h1>
-        <blockquote className="mt-10 quote text-lg">
+        <p className="mt-10 text-lg text-zinc-300 opacity-0.4">
           data engineer who is eager to learn and implement highly maintainable solutions
           that support informed decision making and bring about positive impacts
-        </blockquote>
+        </p>
       </div>
     </div>
   );
