1ffe939085a7b1ee62b8bdb4fd7146e966822d86
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-08 20:57:31 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,10 @@
-import { Cover, Footer } from "../components";
+import { Cover, Footer, Content } from "../components";
 
 function Home() {
   return (
     <div>
       <Cover />
-      <article className="prose-styled">
-        {
-          // data.map((value, index) => (
-          //   <Project {...value} key={index} />
-          // ))
-        }
-      </article>
+      <Content />
       <Footer />
     </div>
   );
