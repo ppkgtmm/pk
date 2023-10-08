704a365079042fc5a25f1cb8601fb2e6315ed6a9
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-08 21:00:10 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
-import SocialList from "./social/SocialList";
+import Social from "./social";
 
 function Footer() {
   return (
     <footer className="w-full bg-black">
       <div className="component py-10 lg:py-16 flex justify-end md:justify-between">
         <div className="hidden md:block md:visible">
-          <SocialList />
+          <Social />
         </div>
         <p className="font-light text-white text-lg py-[11px]">© 2023 Pinky Gautam</p>
       </div>
