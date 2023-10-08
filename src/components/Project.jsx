46dc383005694c8eafd350b46919ca69d74061d6
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-08 20:41:31 +0700

    Create Project.jsx

---
+++ src/components/Project.jsx
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+
+function Project(props) {
+  const {
+    name, cover, link, content,
+  } = props;
+
+  return (
+    <div>
+      <div className="flex flex-col">
+        <p className="font-bold text-lg lowercase capitalize text-black">{name}</p>
+        <p className="text-black">{content}</p>
+        <div className="mt-2">
+          <a href={link} className="not-prose uppercase border border-black text-black hover:bg-black hover:text-white capitalize px-4 py-1.5 rounded">read more</a>
+        </div>
+      </div>
+      <div>
+        <img src={cover} alt={name} />
+      </div>
+    </div>
+  );
+}
+Project.propTypes = {
+  name: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  cover: PropTypes.string.isRequired,
+};
+
+export default Project;
