db5f20cc8b2047e03abb738591d282dc48b55fec
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-08 20:42:44 +0700

    Create Link.jsx

---
+++ src/components/social/Link.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+
+function Link(props) {
+  const { to, children } = props;
+
+  return (
+    <a
+      className="text-white hover:bg-white hover:text-black p-3 rounded-full"
+      href={to}
+      target="_blank"
+      rel="noreferrer"
+    >
+      {children}
+    </a>
+  );
+}
+
+Link.propTypes = {
+  to: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Link;
