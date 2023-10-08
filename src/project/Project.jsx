af8a02e41cc3a950c7406086298eb467ef5c85a8
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-08 19:29:07 +0700

    Update Project.jsx

--- src/project/Project.jsx
+++ src/project/Project.jsx
@@ -2,29 +2,29 @@ import PropTypes from 'prop-types';
 
 function Project(props) {
   const {
-    name, image, link,
-  } = props; /* skills, linkText, content, */
+    name, cover, link, content,
+  } = props;
 
   return (
     <div>
-      <div>
-        <img src={image} alt={name} />
+      <div className="flex flex-col">
+        <p className="font-bold text-lg lowercase capitalize text-gray-950">{name}</p>
+        <p className="text-gray-950">{content}</p>
+        <div className="mt-2">
+          <a href={link} className="not-prose uppercase border border-gray-950 text-gray-950 hover:bg-gray-950 hover:text-white capitalize px-4 py-1.5 rounded">read more</a>
+        </div>
       </div>
       <div>
-        <p className="font-bold text-lg lowercase my-2">{name}</p>
-        <a href={link} className="border border-black hover:bg-black hover:text-white capitalize px-4 py-1.5 rounded">read more</a>
+        <img src={cover} alt={name} />
       </div>
     </div>
   );
 }
-
 Project.propTypes = {
   name: PropTypes.string.isRequired,
-  // skills: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
-  // linkText: PropTypes.string.isRequired,
-  // content: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  cover: PropTypes.string.isRequired,
 };
 
 export default Project;
