d27e5cfdbd4ac9a9cb707d9e91a3203ad474ba0e
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-08 20:42:41 +0700

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import SocialList from "./social/SocialList";
+
+function Footer() {
+  return (
+    <footer className="w-full bg-black">
+      <div className="component py-10 lg:py-16 flex justify-end md:justify-between">
+        <div className="hidden md:block md:visible">
+          <SocialList />
+        </div>
+        <p className="font-light text-white text-lg py-[11px]">© 2023 Pinky Gautam</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
