168deb1b11c613d316431c0ebd8da09569cf5b02
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-08 20:57:21 +0700

    Create Content.jsx

---
+++ src/components/Content.jsx
@@ -0,0 +1,36 @@
+import data from "../contents/projects.json";
+
+function Content() {
+  return (
+    <article className="prose-styled">
+      {
+        data.map(
+          ({
+            name, cover, link, content,
+          }) => (
+            <div>
+              <div className="flex flex-col">
+                <p className="font-bold text-lg lowercase capitalize text-black">{name}</p>
+                <p className="text-black">{content}</p>
+                <div className="mt-2">
+                  <a
+                    href={link}
+                    className="not-prose uppercase border border-black text-black hover:bg-black \
+                    hover:text-white capitalize px-4 py-1.5 rounded"
+                  >
+                    read more
+                  </a>
+                </div>
+              </div>
+              <div>
+                <img src={cover} alt={name} />
+              </div>
+            </div>
+          ),
+        )
+      }
+    </article>
+  );
+}
+
+export default Content;
