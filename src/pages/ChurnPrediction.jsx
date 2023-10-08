86370a64a4a235ba8808f66bd5e1aa7245417065
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-08 20:41:59 +0700

    Update ChurnPrediction.jsx

--- src/pages/ChurnPrediction.jsx
+++ src/pages/ChurnPrediction.jsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from 'react';
-import Markdown from 'react-markdown';
-import contentPath from '../contents/churn-prediction.md';
+import { useEffect, useState } from "react";
+import Markdown from "react-markdown";
+import contentPath from "../contents/churn-prediction.md";
 
 function ChurnPrediction() {
-  const [content, setContent] = useState('');
+  const [content, setContent] = useState("");
   useEffect(() => {
     fetch(contentPath).then((response) => response.text()).then((md) => setContent(md));
-    document.title = 'churn prediction';
+    document.title = "churn prediction";
     window.scrollTo({ top: 0, left: 0 });
   });
   return (
