4871eb07d152438072e1669ad0f894ffa3af740c
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-08 21:58:15 +0700

    Update index.js

--- src/contents/index.js
+++ src/contents/index.js
@@ -1,7 +1,9 @@
 import churnPrediction from "./churn-prediction.md";
+import fakeNewsDetection from "./fake-news-detection.md";
 
 const contents = {
   churnPrediction,
+  fakeNewsDetection,
 };
 
 export default contents;
