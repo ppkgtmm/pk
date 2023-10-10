c3897a2a5b178390739b46559c6efa20635a8058
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-10 23:30:06 +0700

    Update index.js

--- src/contents/index.js
+++ src/contents/index.js
@@ -1,9 +1,11 @@
 import churnPrediction from "./churn-prediction.md";
 import fakeNewsDetection from "./fake-news-detection.md";
+import olistDashboard from "./olist-dashboard.md";
 
 const contents = {
   churnPrediction,
   fakeNewsDetection,
+  olistDashboard,
 };
 
 export default contents;
