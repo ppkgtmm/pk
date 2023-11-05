7e49ffe4f247063781ff6d671c65f690fdfacd64
Pinky Gautam <pkgtm1998@gmail.com>
2023-11-05 21:52:59 +0700

    Update index.js

--- src/contents/index.js
+++ src/contents/index.js
@@ -1,11 +1,13 @@
 import churnPrediction from "./churn-prediction.md";
 import fakeNewsDetection from "./fake-news-detection.md";
 import olistDashboard from "./olist-dashboard.md";
+import hotelBookings from "./hotel-bookings-etl.md";
 
 const contents = {
   churnPrediction,
   fakeNewsDetection,
   olistDashboard,
+  hotelBookings,
 };
 
 export default contents;
