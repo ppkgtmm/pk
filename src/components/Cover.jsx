13a755a79b292bba8626676bdeb134506c87b29d
Pinky Gautam <pkgtm1998@gmail.com>
2023-11-11 19:51:51 +0700

    Update Cover.jsx

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -2,7 +2,7 @@ import Social from "./social";
 
 function Cover() {
   return (
-    <div className="bg-black w-full flex flex-col text-white min-h-[80vh]">
+    <div className="bg-black w-full flex flex-col text-white min-h-[75vh] md:min-h-[80vh]">
       <div className="component flex flex-col my-auto py-20">
         <h1 className="text-[6ch] md:text-[8ch] lg:text-[9ch] font-bold leading-[1.5ch]">
           Pinky
