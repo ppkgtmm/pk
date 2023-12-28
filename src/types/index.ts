34bf4b501e364fbfe819210192a7407630bdcdf5
Pinky Gautam <pinky@ddproperty.com>
2023-12-29 00:23:41 +0700

    add types

---
+++ src/types/index.ts
@@ -0,0 +1,41 @@
+type ConfigObject = Record<string, string>
+
+interface Config {
+  profile: ConfigObject
+  blog: ConfigObject
+  since: number
+  isProd: boolean
+  // lang: string
+}
+
+export enum category {
+  PROJ = '📊 project',
+  ARTI = '📝 article'
+}
+
+export enum tag {
+  KFK = 'kafka',
+  SPK = 'spark',
+  ARF = 'airflow',
+  TBL = 'tableau',
+  ML = 'machine learning',
+  VIZ = 'visualization',
+  DENG = 'data engineering'
+}
+
+export interface TPost {
+  id: number
+  title: string
+  link: string
+  category: category
+  tags: tag[]
+  summary: string
+  thumbnail: string
+  start_date: string
+}
+
+export type TPosts = TPost[]
+export type TTags = tag[]
+export type TCategories = string[]
+
+export type { Config }
