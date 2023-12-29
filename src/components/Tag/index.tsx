0360af91664030356ca55c15bf8b7cf7ca48b6d4
Pinky Gautam <pinky@ddproperty.com>
2023-12-29 15:33:39 +0700

    Update index.tsx

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,19 +1,31 @@
 import styled from '@emotion/styled'
-// import { useRouter } from 'next/router'
 import React from 'react'
 import { colors } from '../../styles'
+import { COLOR_SET } from './constants'
+
+export const getColorClassByName = (name: string): string => {
+  try {
+    let sum = 0
+    name.split('').forEach((alphabet) => (sum = sum + alphabet.charCodeAt(0)))
+    const colorKey = sum
+      .toString(8)
+      ?.[sum.toString(8).length - 1].toUpperCase()
+    return COLOR_SET[colorKey]
+  } catch {
+    return COLOR_SET[0]
+  }
+}
 
 interface Props {
   children: string
 }
 
 const Tag: React.FC<Props> = ({ children }) => {
-  // const router = useRouter()
-
-  // const handleClick = (value: string) => {
-  //   router.push(`/?tag=${value}`)
-  // }
-  return <StyledWrapper>{children}</StyledWrapper>
+  return (
+    <StyledWrapper style={{ backgroundColor: getColorClassByName(children) }}>
+      {children}
+    </StyledWrapper>
+  )
 }
 
 export default Tag
@@ -27,8 +39,7 @@ const StyledWrapper = styled.div`
   font-size: 0.75rem;
   line-height: 1rem;
   font-weight: 400;
-  color: ${colors.gray10};
-  background-color: ${colors.gray3};
+  color: ${colors.gray12};
   cursor: pointer;
   white-space: nowrap;
 `
