f809bab16cf0426c08406598672130c723c24f52
Pinky Gautam <pkgtm1998@gmail.com>
2024-04-13 01:23:03 +0700

    Create notes.ts

---
+++ src/constants/notes.ts
@@ -0,0 +1,22 @@
+import type { TNotes } from '../types'
+
+const notes: TNotes = [
+  {
+    title: 'ACID Properties',
+    slug: 'acid'
+  },
+  {
+    title: 'BASE Properties',
+    slug: 'base'
+  },
+  {
+    title: 'CAP and PACELC Theorem',
+    slug: 'cap'
+  },
+  {
+    title: 'Data Partitioning',
+    slug: 'partition'
+  }
+]
+
+export { notes }
