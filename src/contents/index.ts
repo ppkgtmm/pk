9ce6e31ab1af0e96cdb4bad72da5695ea2037929
Pinky Gautam <pkgtm1998@gmail.com>
2024-04-13 01:23:07 +0700

    Update index.ts

--- src/contents/index.ts
+++ src/contents/index.ts
@@ -3,6 +3,9 @@ import fakeNewsDetection from './projects/fake-news-detection.md'
 import olistDashboard from './projects/olist-dashboard.md'
 import hotelBookings from './projects/hotel-bookings-etl.md'
 import acid from './notes/acid.md'
+import base from './notes/base.md'
+import cap from './notes/cap.md'
+import partition from './notes/partition.md'
 
 const projects: Record<string, string> = {
   'churn-prediction': churnPrediction,
@@ -12,7 +15,10 @@ const projects: Record<string, string> = {
 }
 
 const notes: Record<string, string> = {
-  acid
+  acid,
+  base,
+  cap,
+  partition
 }
 
 export { projects, notes }
