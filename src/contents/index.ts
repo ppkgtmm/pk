bc255a94e4c571f47176f195209a504674c43c13
Pinky Gautam <pkgtm1998@gmail.com>
2024-04-12 23:52:30 +0700

    Update index.ts

--- src/contents/index.ts
+++ src/contents/index.ts
@@ -3,7 +3,6 @@ import fakeNewsDetection from './projects/fake-news-detection.md'
 import olistDashboard from './projects/olist-dashboard.md'
 import hotelBookings from './projects/hotel-bookings-etl.md'
 
-const contents: Record<string, string> = {
 const projects: Record<string, string> = {
   'churn-prediction': churnPrediction,
   'fake-news-detection': fakeNewsDetection,
