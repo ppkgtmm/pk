b0cc7eb88ef1ebd26d019b732fb08d35907a52af
Pinky Gautam <pkgtm1998@gmail.com>
2024-06-09 00:17:30 +0700

    Update projects.ts

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,67 +1,70 @@
-import type { Config, TCategories, TTags, TPosts } from '../types'
-import { category, tag } from '../types'
-
-const config: Config = {
-  profile: {
-    name: 'Pinky Gautam',
-    role: 'data engineer',
-    email: 'pinky.gtm@outlook.com',
-    linkedin: 'pinky-gautam',
-    github: 'ppkgtmm',
-    medium: '@ppkgtmm'
-  },
-  since: 2023
-}
-const defaultCategory = '📂 all'
-const categories: TCategories = [defaultCategory, ...Object.values(category)]
-const tags: TTags = Object.values(tag)
-
-const posts: TPosts = [
+export const contact = [
   {
-    title: 'hotel bookings ETL',
-    slug: 'hotel-bookings-etl',
-    summary:
-      'Designed and developed data warehouse from realistic hotel reservation data',
-    tags: [tag.DENG, tag.VIZ, tag.KFK, tag.SPK, tag.ARF, tag.TBL],
-    category: category.PROJ,
-    thumbnail: './imgs/hotel-bookings/booking-dashboard.png',
-    id: 4,
-    start_date: 'may 2023'
+    icon: '📩',
+    text: 'Email',
+    link: 'mailto:pinky.gtm@outlook.com'
   },
   {
-    title: 'olist e-commerce dashboard',
-    slug: 'olist-dashboard',
-    summary:
-      'Built dashboard to summarize revenue, customer satisfaction and delivery service',
-    tags: [tag.VIZ, tag.TBL],
-    category: category.PROJ,
-
-    thumbnail: './imgs/olist/dashboard.png',
-    id: 3,
-    start_date: 'jun 2023'
+    icon: '👥',
+    text: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/pinky-gautam/'
   },
   {
-    title: 'churn prediction',
-    slug: 'churn-prediction',
-    summary:
-      'Utilized machine learning to identify customers who are at risk of churning',
-    tags: [tag.ML, tag.VIZ, tag.ARF],
-    category: category.PROJ,
-
-    thumbnail: './imgs/churn/front-end.png',
-    id: 2,
-    start_date: 'mar 2022'
-  },
-  {
-    title: 'fake news detection',
-    slug: 'fake-news-detection',
-    summary:
-      'Implemented machine learning model to automate fake news detection',
-    tags: [tag.ML, tag.VIZ, tag.SPK],
-    category: category.PROJ,
-    thumbnail: './imgs/fake-news/front-end.png',
-    id: 1,
-    start_date: 'mar 2022'
+    icon: '💻',
+    text: 'Github',
+    link: 'https://github.com/ppkgtmm'
   }
 ]
-export { config, categories, tags, posts, defaultCategory }
+
+export const projects = [
+  {
+    heading: 'E-commerce Dashboard',
+    content: 'Prepared initial data into suitable form and built dashboard to summarize e-commerce revenue, customer satisfaction and partner delivery service',
+    thumbnail: 'imgs/olist/dashboard.png',
+    extras: {
+      text: 'Read More',
+      icon: '📊',
+      link: '#/e-commerce-dashboard'
+    }
+  },
+  {
+    heading: 'Churn Prediction',
+    content: 'Implemented a machine learning to identify customers at risk of churning. Served the model predictions through API and web user interface',
+    thumbnail: 'imgs/churn/front-end.png',
+    extras: {
+      text: 'Read More',
+      icon: '💳',
+      link: '#/churn-prediction'
+    }
+  },
+  {
+    heading: 'Fake News Detection',
+    content: 'Developed data cleaning logic and trained a machine learning model to detect fake news based on cleaned input data. Created API and web page to serve predictions',
+    thumbnail: 'imgs/fake-news/front-end.png',
+    extras: {
+      text: 'Read More',
+      icon: '📰',
+      link: '#/fake-news-detection'
+    }
+  },
+  {
+    heading: 'Tourist Attraction Catalogue',
+    content: 'Built responsive web pages to display tourist attractions around the world and the season they are best to be visited in using manually collected data',
+    thumbnail: 'imgs/go-now.png',
+    extras: {
+      text: 'View Project',
+      icon: '🏝️',
+      link: 'https://go-now.netlify.app/'
+    }
+  },
+  {
+    heading: 'Tile Matching Game',
+    content: 'Coded game initialization and scoring logic in a tile matching game. Implemented the game to flip tiles back for incorrect matches and to limit number of mistakes',
+    thumbnail: 'imgs/tile-game.png',
+    extras: {
+      text: 'View Project',
+      icon: '🐻‍❄️',
+      link: 'https://ppkgtmm.github.io/recaller/'
+    }
+  }
+]
