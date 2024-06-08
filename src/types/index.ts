4868b26204cc158bb5b7bb6c7ed518d66fa1b6e1
Pinky Gautam <pkgtm1998@gmail.com>
2024-06-09 00:17:14 +0700

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,21 @@
+export interface TContact {
+  icon: string
+  text: string
+  link: string
+}
+
+export interface IProject {
+  heading: string
+  content: string
+  thumbnail: string
+  direction: boolean
+  extras: {
+    text: string
+    icon: string
+    link: string
+  }
+}
+
 type ConfigObject = Record<string, string>
 
 interface Config {
