378b9d1e471b0bcdb9c8fc7d28fa08ed9165fd84
Pinky Gautam <pkgtm1998@gmail.com>
2024-06-09 15:01:18 +0700

    Update index.ts

--- src/contents/index.ts
+++ src/contents/index.ts
@@ -1,13 +1,13 @@
 import churnPrediction from './projects/churn-prediction.md'
 import fakeNewsDetection from './projects/fake-news-detection.md'
 import olistDashboard from './projects/olist-dashboard.md'
-import hotelBookings from './projects/hotel-bookings-etl.md'
+import hotelDataWarehouse from './projects/hotel-data-warehouse.md'
 
 const projects: Record<string, string> = {
   'churn-prediction': churnPrediction,
   'fake-news-detection': fakeNewsDetection,
   'e-commerce-dashboard': olistDashboard,
-  'hotel-data-warehouse': hotelBookings
+  'hotel-data-warehouse': hotelDataWarehouse
 }
 
 export { projects }
