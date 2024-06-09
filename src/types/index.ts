b8d3f8209151b7d61952514e13d962c502722cc9
Pinky Gautam <pkgtm1998@gmail.com>
2024-06-09 14:26:58 +0700

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,7 +4,7 @@ export interface TContact {
   link: string
 }
 
-export interface IProject {
+export interface TProject {
   heading: string
   content: string
   thumbnail: string
@@ -15,50 +15,3 @@ export interface IProject {
     link: string
   }
 }
-
-type ConfigObject = Record<string, string>
-
-interface Config {
-  profile: ConfigObject
-  since: number
-}
-
-export enum category {
-  PROJ = '📊 project',
-  ARTI = '📝 article'
-}
-
-export enum tag {
-  KFK = 'kafka',
-  SPK = 'spark',
-  ARF = 'airflow',
-  TBL = 'tableau',
-  ML = 'machine learning',
-  VIZ = 'visualization',
-  DENG = 'data engineering',
-  // CON = 'concepts',
-  // LIFE = 'life',
-  // STU = 'students',
-  // GRD = 'graduation',
-  // SCR = 'scripting',
-  // LNX = 'linux',
-  // PLT = 'platform'
-}
-
-export interface TPost {
-  id: number
-  title: string
-  slug?: string
-  url?: string
-  category: category
-  tags: tag[]
-  summary: string
-  thumbnail: string
-  start_date: string
-}
-
-export type TPosts = TPost[]
-export type TTags = tag[]
-export type TCategories = string[]
-
-export type { Config }
